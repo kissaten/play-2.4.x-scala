22cc137e4744cdb59eea9f9cadba23d242cfc316
Joe Kutner <jpkutner@gmail.com>
2015-07-01 11:57:00 -0500

    Added header logging

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,7 +5,13 @@ import play.api.mvc._
 
 object Application extends Controller {
 
-  def index = Action {
+  def index = Action { request =>
+    // print the request debug information
+    println(s"*** content-type: ${request.contentType}")
+    println(s"*** headers: ${request.headers}")
+    println(s"*** body: ${request.body}")
+    println(s"*** query string: ${request.rawQueryString}")
+
     Ok(views.html.index("Your new application is ready."))
   }
 
